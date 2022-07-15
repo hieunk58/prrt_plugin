cdf4b1c360fd527cdc05db40ff9c851e8d8db830
hieunk <khachieunk@gmail.com>
2022-07-15 23:05:53 +0200

    constructor function init

--- plugin/src/gstprrtsrc.c
+++ plugin/src/gstprrtsrc.c
@@ -1,6 +1,7 @@
 #include "gstprrtsrc.h"
 
 static void gst_prrtsrc_class_init (GstPRRTSrcClass *klass);
+static void gst_prrtsrc_init (GstPRRTSrc *prrt_src);
 
 static GstStaticPadTemplate src_factory =
 GST_STATIC_PAD_TEMPLATE (
@@ -21,6 +22,21 @@ static void gst_prrtsrc_class_init (GstPRRTSrcClass *klass) {
 
     // add pad template
     gst_element_class_add_static_pad_template (element_class, &src_factory);
+}
+
+/* _init() function is used to initialize a specific instance of prrtsrc type */
+static void gst_prrtsrc_init (GstPRRTSrc *prrt_src) {
+    GST_DEBUG ("gst_prrtsrc_init");
+
+    // TODO set some default values for plugin
+
+    /* configure basesrc to be a live source */
+  gst_base_src_set_live (GST_BASE_SRC (prrt_src), TRUE);
+  /* make basesrc output a segment in time */
+  gst_base_src_set_format (GST_BASE_SRC (prrt_src), GST_FORMAT_TIME);
+  /* make basesrc set timestamps on outgoing buffers based on the running_time
+   * when they were captured */
+  gst_base_src_set_do_timestamp (GST_BASE_SRC (prrt_src), TRUE);
 }
 
 /* plugin_init is a special function which is called
