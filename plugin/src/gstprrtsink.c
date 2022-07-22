e3a63a736fe952cb40b394588a62e52a3dfd08e8
hieunk <khachieunk@gmail.com>
2022-07-22 23:17:33 +0200

    sink plugin init

--- plugin/src/gstprrtsink.c
+++ plugin/src/gstprrtsink.c
@@ -7,3 +7,29 @@ GST_STATIC_PAD_TEMPLATE (
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS_ANY
 );
+
+static gboolean prrtsink_init (GstPlugin *prrtsink) {
+    return gst_element_register(prrtsink, "prrtsink",
+        GST_RANK_NONE,
+        GST_TYPE_PRRTSINK);
+}
+
+#ifndef PACKAGE
+#define PACKAGE "gst-prrt"
+#endif
+
+#ifndef VERSION
+#define VERSION "1.0"
+#endif
+
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    prrtsink,
+    "a plugin transfers data using PRRT protocol",
+    prrtsink_init,
+    VERSION,
+    "MIT",
+    "none",
+    "https://github.com/hieunk58/prrt_plugin"
+)
