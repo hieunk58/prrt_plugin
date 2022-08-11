758b4618ddaa16f1e42575b93070131b6e6ece90
hieunk <khachieunk@gmail.com>
2022-08-11 23:47:29 +0200

    define custom data

--- apps/gst_receiver_udp_prrt_h264.c
+++ apps/gst_receiver_udp_prrt_h264.c
@@ -1,7 +1,25 @@
 #include <gst/gst.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+
+typedef struct _CustomData {
+    GstElement *pipeline;
+    GstElement *video_mixer;
+    GstElement *video_convert;
+    GstElement *video_sink;
+    GstElement *queue_video;
+
+    // UDP
+    GstElement *udpsrc;
+    GstElement *h264parser_udp;
+    GstElement *decoder_udp;
+    GstElement *queue_udp;
+
+    // PRRT
+    GstElement *prrtsrc;
+    GstElement *h264parser_prrt;
+    GstElement *decoder_prrt;
+    GstElement *queue_prrt;
+
+} CustomData;
 
 static gboolean
 bus_call (GstBus *bus, GstMessage *msg, gpointer data) {
